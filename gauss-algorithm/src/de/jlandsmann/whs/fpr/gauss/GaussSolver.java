d1cdd6aeb1cc9effacf2ebc6004afb69b7a82241
jlandsmann <git.landsmann@gmail.com>
2020-11-20 10:18:54 +0100

    #2 - init project

---
+++ gauss-algorithm/src/de/jlandsmann/whs/fpr/gauss/GaussSolver.java
@@ -0,0 +1,14 @@
+package de.jlandsmann.whs.fpr.gauss;
+
+public class GaussSolver {
+    public double[] solve(double[][] a, double[] b) {
+        if (a.length != b.length) {
+            throw new IllegalArgumentException();
+        }
+        double[] x = new double[b.length];
+
+        
+
+        return x;
+    }
+}
---
+++ gauss-algorithm/src/de/jlandsmann/whs/fpr/gauss/Main.java
@@ -0,0 +1,8 @@
+package de.jlandsmann.whs.fpr.gauss;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
